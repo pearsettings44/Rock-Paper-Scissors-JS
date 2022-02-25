72dd2a1dbb63c605f9e5658507bc2bef886b3567
pearsettings44 <jose.a.pereira03@gmail.com>
2022-02-25 12:30:35 +0000

    Add playRound function.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const rps = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
-    return rps[Math.floor(Math.random() * rps.length)]
+  return rps[Math.floor(Math.random() * rps.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    return `You both played ${playerSelection}! IT'S A DRAW!`;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "Computer played paper! YOU LOST!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "Computer played scissors! YOU WON!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "Computer played paper! YOU WON!";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "Computer played rock! YOU LOST!";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "Computer played scissors! YOU LOST!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "Computer played rock! YOU WON!";
+  }
 }
